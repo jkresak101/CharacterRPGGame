f9bf14d7959a7b85d99b747e75ef6ca66b7469c1
jkresak101 <jkresak101@gmail.com>
2022-09-29 11:52:52 -0700

    initial program

---
+++ CharacterRPGGame/CharacterRPGGame/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine(" Beyond This Door..." +
+    "" +
+    "" +
+    "" +
+    "" +
+    "\n Comes An Adventure...\n " +
+    "" +
+    "" +
+    "" +
+    "" +
+    "Not For The Weak...\n " +
+    "" +
+    "" +
+    "" +
+    "" +
+    "Are You Up For This Dangerous Task?");
+Console.WriteLine(" Type Your Answer");
+var input = Console.ReadLine();
+if (input == "Yes")
+{
+    Console.WriteLine("");
+}
+
+else
+        {
+            Console.WriteLine("I Knew You Weren't A True Adventuerer...");
+        }
